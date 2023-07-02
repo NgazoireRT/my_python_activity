a900b6627189ad16488be385dfff6edda91c182b
Robert Takenzire <81983323+NgazoireRT@users.noreply.github.com>
2023-07-02 10:07:55 +0100

    Add files via upload

---
+++ my_first_code.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is my first line of code
+
+print("My name is Robert Takenzire")
+print("The date is 02/07/2023")
+
+
+# In[3]:
+
+
+# Write a program to analyse real estate data 
+
+#Create Variables. letters correspond to property ID. 
+
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+#total sales for december 
+
+total_sales = a+b+c+d+e 
+
+print(total_sales)
+
+
+# In[5]:
+
+
+# average sales price for the month
+#Calculate the average sales by dividing the total number of sales by 5
+
+avg_sales = total_sales / 5
+
+print(avg_sales)
+
+
+# In[7]:
+
+
+# Qtn 3 - Missing Value for Column C - Property price for C.
+# Sales price is equal to property price + commission. So to solve we subtract commission from sales price in data set provided
+
+property_price_c = (67000 - 6500)
+
+print(property_price_c)
+
+
+# In[ ]:
+
+
+
+
